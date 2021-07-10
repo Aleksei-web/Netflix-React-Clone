576c1d4c9276d5925393dab682d3d94cbb59b9d9
Alex <maksimushkin92@gmail.com>
2021-07-10 11:53:18 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const App = () => {
+	return (
+		<>
+			<Title>Netflix React-app</Title>
+		</>
+	)
+}
+
+const Title = styled.h1`
+	color: blue;
+`
